da894f953de8291309b08199b3d7e667ee1199f8
Michaelballard <MichaelBallard@Michaels-MBP-3.fios-router.home>
2019-07-03 18:20:45 -0400

    Completed Crud Controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -23,7 +23,7 @@ module.exports = {
         };
 
         // create a new job schema and pass it my job object
-        let newJob = new Job(job)
+        let newJob = new Job(job);
 
         // save the new job
         newJob.save((err) => {
@@ -42,10 +42,10 @@ module.exports = {
         Job.find({},(err,jobs) => {
             // if  error occurred  send error with 404 status code
             if(err){
-                return res.status(404).send(err)
+                return res.status(404).send(err);
     
             }
-            return res.status(200).json(jobs)
+            return res.status(200).json(jobs);
         })
         
     },
@@ -55,7 +55,7 @@ module.exports = {
         let id = req.params.id;
 
         if (!id){
-            res.status(400).send({err: 'id is required field'})
+            res.status(400).send({err: 'id is required field'});
         }
 
         Job.findById(id,(err, job)=>{
@@ -65,5 +65,47 @@ module.exports = {
             }
             return res.status(200).send(job)
         })
+    },
+
+    update(req, res){
+        let id = req.params.id;
+        let title = req.body.title;
+        let description = req.body.description;
+
+        let newJob = {};
+
+        if (title){
+            newJob.title = title;
+        }
+
+        if (description){
+            newJob.description = description
+        }
+
+        // call the update method to edit a job
+        Job.updateOne({_id:id}, newJob,(err, result)=>{
+            if(err) {
+                return res.status(500).send(err)
+            }
+            console.log(description)
+            return res.status(200).json({msg:`The job with id ${id} has been updated`});
+        })
+
+    },
+
+    delete(req, res){
+        // get the id from req params
+        let id = req.params.id ;
+    
+        // call findByIdAndRemove method
+        Job.findByIdAndRemove(id, (err)=>{
+            if(err){
+                // if error comes then send 500 status with error
+                console.error(err)
+                return res.status(500).send(err)
+            }
+             // if ok, send msg of deletion
+            return res.status(200).json({msg:'The Job has been deleted'})
+        })   
     }
-}
+};
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
-
 const jobController = require('../controllers/jobsController')
-
 
 router.get('/', jobController.findAll);
 
 router.post('/', jobController.create);
 
 router.get('/:id', jobController.find);
+
+router.put("/:id", jobController.update);
+
+router.delete("/:id", jobController.delete);
 
 module.exports = router;
 
